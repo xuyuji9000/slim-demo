096283a6fd1e1dde8dfc0d249ea8ae6ef247111e
yogiman <xuyuji9000@163.com>
2017-02-06 09:58:21 +0800

    add new files

--- src/config/settings.php
+++ src/config/settings.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'setting' => [
+    'settings' => [
         'displayErrorDetails' => true,  //set false in production
         'addContentLengthHeader' => false,  //Allow the web server to send the content-length header
     ],
