88df26715e18fece73b48d0459fd2804c14b0a9f
Karl Xu <karl.yogi.xu@gmail.com>
2017-02-09 13:49:28 +0800

    add a json api

--- config/routes.php
+++ config/routes.php
@@ -4,3 +4,15 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/home', App\Controllers\FileController::class.':home');
+
+$app->get('/json', function(){
+    header("Content-Type: application/json");
+    $result = [
+        'one'=>1,
+        'three'=>3,
+        'five'=>5
+    ];
+
+    echo json_encode($result);
+    exit;
+});
